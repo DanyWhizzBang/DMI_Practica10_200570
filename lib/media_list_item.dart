6a705c599c972ca72ff251fd85d49c1a673be9f3
DanyWhizzBang <dniarroyo18@gmail.com>
2023-10-06 14:19:04 -0600

    UPDATE3

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -36,7 +36,7 @@ class MediaListItem extends StatelessWidget {
                 ),
                 Positioned(
                   left: 10.0,
-                  bottom: 30.0,
+                  bottom: 10.0,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -45,11 +45,52 @@ class MediaListItem extends StatelessWidget {
                           media.title,
                           style: TextStyle(
                               fontWeight: FontWeight.bold, color: Colors.white),
+                              softWrap: false,
+                              overflow: TextOverflow.ellipsis,
+
                         ),
+                      ),
+                      Container(
+                        width: 300.0,
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: new Text(
+                          media.getGenres(),
+                          style: new TextStyle(color:Colors.white),
+                          maxLines:1,
+                          overflow: TextOverflow.ellipsis,
+
+                        )
                       )
                     ],
                   ),
-                )
+                ),
+                new Positioned(
+                  right: 5.0,
+                  bottom: 10.0,
+                  child: new Column(
+                    children: <Widget>[
+                    new Row(
+                    children: <Widget>[
+                      new Text(media.voteAverage.toString()),
+                      new Container(width: 4.0,),
+                      new Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 16,
+                      )
+                      ]
+                      ),
+                      new Container(height:4.0,),
+                      new Row(
+                        children: <Widget> [
+                          new Text(media.getReleaseYear().toString()),
+                          new Container(width: 4.0,),
+                          new Icon(Icons.date_range, color: Colors.white,size: 16.0,)
+                        ],
+                        )
+                  ],
+                  )
+                  )
               ],
             ),
           )
